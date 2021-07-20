095af364c3dc534d03e5ad0d712fb2f41f24d3ef
JamesGorman <jamie.gorman22@gmail.com>
2021-07-20 15:34:52 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,18 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
-  }
+	  
+	try {
+		String language = args[0];
+		String country = args[1];
+		String name = args[2];
+		Greeter greeter = new Greeter(language, country, name);
+		System.out.println(greeter.sayHello());
+		
+	}catch (ArrayIndexOutOfBoundsException e) {
+		System.out.println("Missing Arguements!!!");
+	}
+
+
+}
 }
